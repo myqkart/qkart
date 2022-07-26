803abbedeabe0422efc96a1e0cd9d183af4ced2a
myqkart <yshop674@gmail.com>
2022-07-27 00:30:07 +0530

    gpay try...

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
       const params = {
         submit_type: "pay",
         mode: "payment",
-        payment_method_types: ["card"],
+        payment_method_types: ["card", "acss_debit"],
         billing_address_collection: "auto",
         shipping_options: [{ shipping_rate: "shr_1LPmQZSG7KneWlvmL92ILvl4" }],
         line_items: req.body.map((item) => {
