c4869ec832303bd20a5acd35d5fc91afd250a4a6
myqkart <yshop674@gmail.com>
2022-07-29 11:26:33 +0530

    email service

---
+++ pages/api/contact.js
@@ -0,0 +1,23 @@
+let nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 25,
+  auth: {
+    user: process.env.NODEMAIL_EMAIL,
+    pass: process.env.NODEMAIL_PASS,
+  },
+});
+
+export default function (req, res) {
+  const mailData = {
+    to: "yshop674@gmail.com",
+    from: req.body.email,
+    subject: "Q-Kart support",
+    text: req.body.message,
+    html: `<div>${req.body.message}</div>`,
+  };
+
+  transporter.sendMail(mailData);
+
+  return res.json({ status: true });
+}
--- pages/help/ContactUs.js
+++ pages/help/ContactUs.js
@@ -2,10 +2,44 @@ import React from "react";
 import { useStateContext } from "../../context/StateContext";
 
 import { AiOutlineMail, AiOutlinePhone } from "react-icons/ai";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
 import Link from "next/link";
 
 const ContactUs = () => {
   const { showSuceess, setShowSuceess } = useStateContext();
+
+  const initialValues = {
+    name: "",
+    email: "",
+    number: "",
+    message: "",
+  };
+  const onSubmit = async (values) => {
+    const res = await fetch("/api/contact", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    });
+
+    if (res.status === 200) {
+      setShowSuceess(true);
+    }
+  };
+
+  const validator = (values) => {
+    const errors = {};
+    if (!values.name) errors.name = "Name required.";
+    else if (!values.email) errors.email = "Email required.";
+    else if (!values.number) errors.number = "Contact number required.";
+    else if (!values.message) errors.message = "Required to submit.";
+
+    return errors;
+  };
+
   return (
     <div>
       {showSuceess ? (
@@ -20,7 +54,7 @@ const ContactUs = () => {
                   Your query has been successfully received. Our team will
                   contact you soon with further information.
                 </p>
-                <div className="btn" onClick={()=>setShowSuceess(false)}>
+                <div className="btn" onClick={() => setShowSuceess(false)}>
                   <Link href={`/`} className="btn">
                     Back to Home
                   </Link>
@@ -64,53 +98,82 @@ const ContactUs = () => {
                   </div>
                   <div className="card col-4">
                     <div className="p-3">
-                      <form id="contactForm">
-                        <div className="form-group">
-                          <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Your Name"
-                            trim="blur"
-                          />
-                        </div>
-                        <br />
-                        <div className="form-group">
-                          <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Your Email"
-                            trim="blur"
-                          />
-                        </div>
-                        <br />
-                        <div className="form-group">
-                          <div className="d-flex mobile_number">
-                            <input
-                              type="tel"
-                              placeholder="Contact Number"
-                              className="form-control"
-                            />
-                          </div>
-                        </div>
-                        <br />
-                        <div className="form-group">
-                          <textarea
-                            className="form-control"
-                            placeholder="Your Message"
-                            trim="blur"
-                          ></textarea>
-                        </div>
-                        <br />
-                        <div className="form-group">
-                          <button
-                            className="btn"
-                            type="button"
-                            onClick={() => setShowSuceess(true)}
-                          >
-                            Submit
-                          </button>
-                        </div>
-                      </form>
+                      <Formik
+                        initialValues={initialValues}
+                        validate={validator}
+                        onSubmit={onSubmit}
+                      >
+                        {({ isSubmitting }) => (
+                          <Form>
+                            <div className="form-group">
+                              <Field
+                                type="text"
+                                name="name"
+                                className="form-control login-input"
+                                placeholder="Your name"
+                              />
+                              <ErrorMessage
+                                name="name"
+                                className="text-danger"
+                                component="div"
+                              />
+                            </div>
+                            <br />
+                            <div className="form-group">
+                              <Field
+                                type="email"
+                                name="email"
+                                className="form-control login-input"
+                                placeholder="Your email"
+                              />
+                              <ErrorMessage
+                                name="email"
+                                className="text-danger"
+                                component="div"
+                              />
+                            </div>
+                            <br />
+                            <div className="form-group">
+                              <Field
+                                type="tel"
+                                name="number"
+                                className="form-control login-input"
+                                placeholder="Contact number"
+                              />
+                              <ErrorMessage
+                                name="number"
+                                className="text-danger"
+                                component="div"
+                              />
+                            </div>
+
+                            <br />
+                            <div className="form-group">
+                              <Field
+                                as="textarea"
+                                type="text"
+                                name="message"
+                                className="form-control login-input"
+                                placeholder="Your message..."
+                              />
+                              <ErrorMessage
+                                name="message"
+                                className="text-danger"
+                                component="div"
+                              />
+                            </div>
+                            <div className="col-auto">
+                              <button
+                                type="submit"
+                                className="btn btn-sm"
+                                // disabled={isSubmitting}
+                              >
+                                Submit
+                              </button>
+                            </div>
+                          </Form>
+                        )}
+                      </Formik>
                     </div>
                   </div>
                 </div>
