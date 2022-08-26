d79731f89eafd8b62e5d85ac88963fdac56ca9cf
myqkart <110031943+myqkart@users.noreply.github.com>
2022-08-26 14:52:17 +0530

    refurbished

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ const Home = ({products, bannerData}) => {
     <div>
       <HeroBanner heroBanner ={bannerData.length && bannerData[0]}/>
       <div className="products-heading">
-        <h2>BestSelling Products</h2>
-        <p>Speakers of many variations</p>
+        <h2>Best Selling Products</h2>
+        <p>Branded and refurbished both available</p>
       </div>
       <div className="products-container">
         {products?.map((prod) => <Product key={prod._id} product ={prod}/>)}
