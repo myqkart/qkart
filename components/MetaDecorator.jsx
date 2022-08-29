644bc97c8a372b2a1a570fd43448f9788a175faa
myqkart <yshop674@gmail.com>
2022-08-29 14:21:39 +0530

    og meta

--- components/MetaDecorator.jsx
+++ components/MetaDecorator.jsx
@@ -6,7 +6,7 @@ const MetaDecorator = ({ title, description }) => {
     <Helmet>
       {/* <title>Nested Title</title> */}
       <meta name="title" content={title} />
-      <meta name="description" content={description}></meta>
+      <meta property="og:description" content={description} />
     </Helmet>
   );
 };
