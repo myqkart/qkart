3959c1a71b37e54dd998e5310c5c123e9b6ecc42
myqkart <yshop674@gmail.com>
2022-08-29 13:12:29 +0530

    meta title

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import Head from 'next/head'
-import Navbar from './Navbar'
-import Footer from './Footer'
+import React from "react";
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { Helmet } from "react-helmet";
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
-    <div className='layout'>
-        <Head>
-            <title>Q-Kart</title>
-        </Head>
-        <header>
-            <Navbar/>
-        </header>
-        <main className='main-container'>
-            {children}
-        </main>
-        <footer>
-            <Footer/>
-        </footer>
+    <div className="layout">
+      <Helmat>
+        <meta name="title" content="Q-Kart"></meta>
+      </Helmat>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
