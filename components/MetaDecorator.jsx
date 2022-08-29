3aae486e76f22f9c67ab9ba55f5d31e5e30f0fac
myqkart <yshop674@gmail.com>
2022-08-29 14:45:55 +0530

    name added

--- components/MetaDecorator.jsx
+++ components/MetaDecorator.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
-const MetaDecorator = ({ title, description }) => {
+const MetaDecorator = ({ title, description, name }) => {
   return (
     <Helmet>
-      {/* <title>Nested Title</title> */}
+      <title>{name}</title>
       <meta property="og:title" content={title} />
+      <meta property="og:title" content={title} />
+      <meta property="description" content={description} />
       <meta property="og:description" content={description} />
     </Helmet>
   );
